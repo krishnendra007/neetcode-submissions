096f7a97daa1d6e6b76b71480bbe9b0dc65a61a8
Krishnendra Sharma <88177574+krishnendra007@users.noreply.github.com>
2026-04-23 16:20:15 +0530

    Add: count-paths - submission-0

---
+++ Data Structures & Algorithms/count-paths/submission-0.java
@@ -0,0 +1,27 @@
+class Solution {
+
+    int fun(int i, int j, int m, int n, int[][] dp) {
+        if (i >= m || j >= n)
+            return 0;
+
+        if (i == m - 1 && j == n - 1) {
+            return 1;
+        }
+
+        if(dp[i][j]!=-1) return dp[i][j];
+
+        return dp[i][j]= fun(i + 1, j, m, n, dp)+fun(i, j + 1, m, n, dp); 
+
+    }
+
+    public int uniquePaths(int m, int n) {
+        int[][] dp = new int[m][n];
+
+        for (int i = 0; i < m; i++) {
+            Arrays.fill(dp[i], -1);
+        }
+
+        return fun(0, 0, m, n, dp);
+
+    }
+}
